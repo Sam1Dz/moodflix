46b007de93a2b88b2ba1029278243e9e1536b78f
Pratama Dimas <pratamadimas.pw@gmail.com>
2025-02-28 12:43:12 +0700

    Init setup project file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: [
+    js.configs.recommended,
+    ts.configs.recommended,
+    ts.configs.stylistic,
+    prettier
+  ]
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn'
+    }
+  }
 ];
 
 export default eslintConfig;
